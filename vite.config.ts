92dfba7e7252409338745fac0f688c9a8f283e40
Mark Belles <markbelles@gmail.com>
2025-03-19 23:08:19 -0500

    oops

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/443labs.github.io/',
   plugins: [svelte()],
   base: '/blocks-svelte/',
 })
